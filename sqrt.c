d516b2625e68cd2c6545a9c7a1c03287e0a9b36d
Chiller Dragon <chillerdragon@gmail.com>
2019-05-12 14:12:07 +0200

    Remove space

--- sqrt.c
+++ sqrt.c
@@ -218,7 +218,7 @@ float ENIACSqrtInterpolated(unsigned long value)
 double SuccessiveApproximationSqrt(double value)
 {
     double old;
-    double new; 
+    double new;
     double delta;
 
     /* check for valid input value */
